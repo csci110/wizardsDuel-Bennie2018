9c2cf672ace09920c385a3cffe5f8ce2e6f16b54
Bennie Kowalski <kowalskib@dewv.edu>
2018-08-31 16:52:34 +0000

    Updates via sync script

---
+++ loader.js
@@ -0,0 +1,10 @@
+import {game} from "./sgc/sgc.js";
+
+game.preloadImage("floor.png");
+//game.preloadImage("horizontalWall.png");
+//game.preloadImage("verticalWall.png");
+game.preloadImage("marcusSheet.png", 48, 48);
+game.preloadImage("strangerSheet.png", 48, 48);
+game.preloadImage("fireballSheet.png", 48, 48);
+game.preloadImage("marcusSpellSheet.png");
+game.preloadImage("strangerSpellSheet.png");
--- wizardsDuel.js
+++ wizardsDuel.js
@@ -1,1 +1,24 @@
+import {game, Sprite} from "./sgc/sgc.js";
+game.setBackground("floor.png");
+game.showScore = true;
+//just Finshed the first class and Sprite for player
+//------------------------------------------------------------------------------------------------
+class PlayerWizard extends Sprite {
+    constructor () {
+        super();
+        this.name = "Marcus the Wizad";
+        this.setImage("marcusSheet.png");
+        this.width = 48;
+        this.height = 48;
+        this.x = this.width;
+        this.y = this.y;
+    }
+}
+
+let marcus = new PlayerWizard();
+marcus.speed = 0;
+
+
+//--------------------------------------------------------------------------------------------------
+
 
